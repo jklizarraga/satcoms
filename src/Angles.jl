84ce05358b76d917834e65cdcc65e2d284a91119
Juan Lizarraga <jlizarraga@dhcp-10-176-46-197.estec.esa.int>
2018-09-26 17:07:36 +0200

    2D Angles module fully implemented and compatible with Julia v1.0

--- src/Angles.jl
+++ src/Angles.jl
@@ -2,16 +2,88 @@ isdefined(Base, :__precompile__) && __precompile__()
 
 module Angles
 
-  import Base: ==, +, -, *, /, .+, .-, .*, ./, //, ^
-  import Base: promote_rule, promote_type, convert, show, mod
+operationsUnary         = (:+, :-, :rem2pi, :mod2pi)
+operationsBetweenAngles = (:+, :-)
+operationsAngleScalar   = (:*, :/, :÷, :^, :%, :rem, :mod)
+operationsScalarAngle   = (:*, :\)
+operationsComparison    = (Symbol("=="), :≠, :<, :≤, :>, :≥)
+operationsTrigonometric = (:sin, :cos, :tan, :cot, :sec, :csc, :sinc, :cosc, :sinpi, :cospi)
 
-  immutable Degrees{T::Number} <: Number
-    val::T
-  end
+operations = unique((operationsUnary..., operationsBetweenAngles..., operationsAngleScalar..., operationsScalarAngle..., operationsComparison..., operationsTrigonometric...))
 
-  immutable Radians{T::Number} <: Number
-    val::T
-  end
+for op in operations
+  @eval import Base: $op
+  @eval export $op
+end
 
-  
+import Base: convert, promote_rule, show
+
+abstract type Angle end
+
+asciiRepresentation = Dict("Degrees"=>"º"   , "Radians"=>"rad")
+ htmlRepresentation = Dict("Degrees"=>"&deg", "Radians"=>"rad")
+
+for angularUnits in (:Degrees, :Radians)
+  @eval begin
+          export $angularUnits
+
+          struct $angularUnits{T<:Number} <: Angle
+            val::T
+          end
+
+          convert(::Type{T}               , x::$angularUnits{T}) where {T<:Number}           = x.val
+          convert(::Type{$angularUnits{T}}, x::$angularUnits{S}) where {T<:Number,S<:Number} = $angularUnits(T(x.val))
+
+          promote_rule(::Type{$angularUnits{T}},::Type{$angularUnits{S}}) where {T<:Number,S<:Number} = $angularUnits{promote_type(T,S)}
+
+          # Pretty printing
+          Base.show(io::IO,                     x::$angularUnits{T}) where {T} = print(io, x.val, asciiRepresentation[$(String(angularUnits))])
+          Base.show(io::IO, ::MIME"text/plain", x::$angularUnits{T}) where {T} = print(io, "Angle in ",$(String(angularUnits)),"{$T}: ", x, "\n")
+          Base.show(io::IO, ::MIME"text/html" , x::$angularUnits{T}) where {T} = print(io, x.val, htmlRepresentation[$(String(angularUnits))] ," [<code>",$(String(angularUnits)),"{$T}</code>]")
+        end
+end
+
+# Conversion between the different angles.
+convert(::Type{Radians{T}}, x::Degrees) where {T<:Number} = Radians(x.val * (π / 180.))
+convert(::Type{Degrees{T}}, x::Radians) where {T<:Number} = Degrees(x.val * (π \ 180.))
+
+promote_rule(::Type{Degrees{T}},::Type{Radians{S}}) where {T<:Number,S<:Number} = Radians{promote_type(T,S)}
+
+# New methods for the operations.
+
+## Arithmetic operators
+### Unary operators
++(x::T) where {T<:Angle} = x
+-(x::T) where {T<:Angle} = T(-x.val)
+
+rem2pi(x::T, r::RoundingMode) where {T<:Angle} = T(rem2pi(x.val,r))
+mod2pi(x::T                 ) where {T<:Angle} = T(mod2pi(x.val  ))
+
+### Operations between angles
+for op in operationsBetweenAngles
+  @eval $op(x::T, y::T) where {T<:Angle         } = T($op(x.val, y.val))
+  @eval $op(x::T, y::S) where {T<:Angle,S<:Angle} = $op(promote(x,y)...)
+end
+
+### Operations with scalars
+for op in operationsAngleScalar
+  @eval $op(x::T, scalar::Number) where {T<:Angle} = T($op(x.val, scalar))
+end
+
+for op in operationsScalarAngle
+  @eval $op(scalar::Number, x::T) where {T<:Angle} = T($op(scalar,x.val))
+end
+
+## Comparison (between angles) operations
+for op in operationsComparison
+  @eval $op(x::T    , y::T    ) where {T<:Angle} = $op(x.val,y.val)
+  @eval $op(x::Angle, y::Angle)                  = $op(promote(x,y)...)
+end
+
+## Trigonometric functions
+for fun in operationsTrigonometric
+  @eval $fun(x::Radians) = $fun(x.val)
+  @eval $fun(x::Degrees) = $(Symbol(fun,'d'))(x.val)
+end
+
 end
--- src/notebook.jl
+++ src/notebook.jl
@@ -1,3 +1,102 @@
+# METAPROGRAMMING
+#
+# @eval $(ex) == eval(ex)
+#
+#
+# AST: (call f x) -> Expr(:call, :f, :x)
+# import Base: x, y	-> AST: (import (: (. Base) (. x) (. y))) -> Expr(:import, (:(:), (:., :Base), (:., :x), (:., :y)))
+
+operations = ("+","-")
+
+ex1 = :(import Base: +)
+eval(ex1)
+ex2 = :(import Base: $(:+))
+eval(ex2)
+ex3 = :(import Base: $(join(operations,", ")))
+eval(ex3)
+ex4 = :(import Base: +,-)
+eval(ex4)
+ex5 = :(import Base: $((:+,:-)...))
+eval(ex5)
+ex6 = :(import Base: $(:+),$(:-))
+eval(ex6)
+
+macroexpand(Main, quote @eval import Base: +, - end)
+macroexpand(Main, quote @eval $(ex4) end)
+macroexpand(Main, quote @eval $(ex6) end)
+
+import Base: $((:+,:-))
+
+# struct Degrees{T<:Number} <: Angle
+#   val::T
+# end
+#
+# convert(::Type{T}         , x::Degrees{T}) where {T<:Number}           = x.val
+# convert(::Type{Degrees{T}}, x::Degrees{S}) where {T<:Number,S<:Number} = Degrees(T(x.val))
+#
+# promote_rule(::Type{Degrees{T}},::Type{Degrees{S}}) where {T<:Number,S<:Number} = Degrees{promote_type(T,S)}
+#
+# struct Radians{T<:Number} <: Angle
+#   val::T
+# end
+#
+# convert(::Type{T}         , x::Radians{T}) where {T<:Number}           = x.val
+# convert(::Type{Radians{T}}, x::Radians{S}) where {T<:Number,S<:Number} = Radians(T(x.val))
+#
+# promote_rule(::Type{Radians{T}},::Type{Radians{S}}) where {T<:Number,S<:Number} = Radians{promote_type(T,S)}
+
+#
+# for op in (:+, :-)
+#   @eval $op(x::Radians, y::Radians) = Radians($op(x.val, y.val))
+#   @eval $op(x::Degrees, y::Degrees) = Degrees($op(x.val, y.val))
+# end
+#
+# for op in (:*, :/, :÷, :^)
+#   @eval $op(x::Radians, scalar::Number) = Radians($op(x.val, scalar))
+#   @eval $op(x::Degrees, scalar::Number) = Degrees($op(x.val, scalar))
+# end
+#
+# for op in (:*, :\)
+#   @eval $op(scalar::Number, x::Radians) = Radians($op(scalar,x.val))
+#   @eval $op(scalar::Number, x::Degrees) = Degrees($op(scalar,x.val))
+# end
+import Base: ==, +, -, *, /, ÷, ^, \, %, rem, mod, rem2pi, mod2pi
+import Base: sin, cos, tan, cot, sec, csc, sinc, cosc, sinpi, cospi
+import Base: convert, promote_rule, show
+
+abstract type Angle end
+
+asciiRepresentation = Dict("Degrees"=>"º"   , "Radians"=>"rad")
+ htmlRepresentation = Dict("Degrees"=>"&deg", "Radians"=>"rad")
+
+for angularUnits in (:Degrees, :Radians)
+  angularUnits = :Degrees
+  macroexpand(Main,
+    quote @eval begin
+                  struct $angularUnits{T<:Number} <: Angle
+                    val::T
+                  end
+
+                  convert(::Type{T}               , x::$angularUnits{T}) where {T<:Number}           = x.val
+                  convert(::Type{$angularUnits{T}}, x::$angularUnits{S}) where {T<:Number,S<:Number} = $angularUnits(T(x.val))
+
+                  promote_rule(::Type{$angularUnits{T}},::Type{$angularUnits{S}}) where {T<:Number,S<:Number} = $angularUnits{promote_type(T,S)}
+
+                  Base.show(io::IO,                     x::$angularUnits{T}) where {T} = print(io, x.val, asciiRepresentation[$(String(angularUnits))])
+                  Base.show(io::IO, ::MIME"text/plain", x::$angularUnits{T}) where {T} = print(io, "Angle in ",$(String(angularUnits)),"{$T}: ", x, "\n")
+                  Base.show(io::IO, ::MIME"text/html" , x::$angularUnits{T}) where {T} = print(io, x.val, htmlRepresentation[$(String(angularUnits))] ," [<code>",$(String(angularUnits)),"{$T}</code>]")
+              end
+    end)
+end
+
+using Main.Angles
+x = Degrees(5)
+y = Degrees(5.0)
+z = Radians(0.03)
+
+x + y
+x + z
+
 include("SIBaseUnits.jl")
 include("SIQuantities.jl")
 using SIBaseUnits
